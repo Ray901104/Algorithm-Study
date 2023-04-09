0ea017659ecbb9adea2a5fec6abf322e4d984ed2
Youngrae Cho <ip323@naver.com>
2023-04-09 17:34:45 +0900

    달리기 경주 - Level 1

--- src/main/java/programmers/level1/Race.java
+++ src/main/java/programmers/level1/Race.java
@@ -1,28 +1,44 @@
 package programmers.level1;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Race
 {
     public String[] solution(String[] players, String[] callings)
     {
-        String[] answer = {};
+        String firstPlayer = players[0]; // 1위 선수
+        Map<String, Integer> playersMap = new HashMap<>();
 
-        for (int i = 0; i < callings.length; i++)
+        // players Map으로 옮기기
+        for (int i = 0; i < players.length; i++)
+            playersMap.put(players[i], i);
+
+        for (String calling : callings)
         {
-            // 호출된 선수의 위치를 앞 선수와 바꾸기
-            String tmp = "";
-            int fastPlayerIdx = Arrays.asList(players).indexOf(callings[i]); // 추월한 선수의 위치
-            int caughtPlayerIdx =  fastPlayerIdx - 1; // 추월 당한 선수의 위치
+            // 호출리스트의 선수가 현재 1위인 경우 스킵
+            if (calling.equals(firstPlayer))
+                continue;
+
+            // 1. 호출된 선수의 위치를 Map으로부터 받아오기
+            int fastPlayerIdx = playersMap.get(calling); // 추월한 선수의 위치
+            int caughtPlayerIdx = fastPlayerIdx - 1; // 추월 당한 선수의 위치
             String fastPlayer = players[fastPlayerIdx]; // 추월한 선수
             String caughtPlayer = players[caughtPlayerIdx]; // 추월 당한 선수
 
-            // players 배열에 바뀐 순위로 정렬
-            players[caughtPlayerIdx] = fastPlayer;
-            players[fastPlayerIdx] = caughtPlayer;
+            // 2. players 배열에 바뀐 순위로 정렬
+            players[caughtPlayerIdx] = fastPlayer; // 추월한 선수
+            players[fastPlayerIdx] = caughtPlayer; // 추월 당한 선수
+
+            // 3. 변경된 위치값을 Map에도 갱신
+            playersMap.put(fastPlayer, caughtPlayerIdx);
+            playersMap.put(caughtPlayer, fastPlayerIdx);
+            
+            // 4. 위치가 바뀐 이후의 1위 선수 갱신
+            firstPlayer = players[0];
         }
-        answer = players;
-        return answer;
+        return players;
     }
 
     public static void main(String[] args)
