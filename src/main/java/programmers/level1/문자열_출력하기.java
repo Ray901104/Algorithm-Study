a44524acea068041ef7be49c3f2c9a6be693133b
Youngrae Cho <ip323@naver.com>
2023-05-22 22:16:51 +0900

    문자열 출력하기 - Level 1

---
+++ src/main/java/programmers/level1/문자열_출력하기.java
@@ -0,0 +1,13 @@
+package programmers.level1;
+
+import java.util.Scanner;
+
+public class 문자열_출력하기
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println("a = " + a);
+    }
+}
