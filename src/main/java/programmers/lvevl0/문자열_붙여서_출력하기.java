b8219849f4e99a5a94f519ce78350e28cc8216f5
Youngrae Cho <ip323@naver.com>
2023-06-05 19:04:52 +0900

    문자열 붙여서 출력하기 - Level 0

---
+++ src/main/java/programmers/lvevl0/문자열_붙여서_출력하기.java
@@ -0,0 +1,15 @@
+package programmers.lvevl0;
+
+import java.util.Scanner;
+
+public class 문자열_붙여서_출력하기
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a + b);
+    }
+}
